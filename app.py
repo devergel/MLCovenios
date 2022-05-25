eb9d8252a64cf075a3dcbe51df65f8a954c03e7a
dvergel <de.vergel10@uniandes.edu.co>
2022-05-25 08:56:55 -0500

    fix

--- app.py
+++ app.py
@@ -98,6 +98,7 @@ def main():
                 
                 df_y_GT = pd.DataFrame(y_pred_GT)
                 aux = pd.concat([seats_index_GT, df_y_GT], axis=1)
+                aux = aux[aux["RelationID"]>0]
                 aux = aux.sort_values(by = [1], ascending = False).head(3)
                 aux["Preferencia"] = "Disponibilidad"
                 
@@ -131,6 +132,7 @@ def main():
                 y_pred_LT = random.predict_proba(df)
                 df_y_LT = pd.DataFrame(y_pred_LT)
                 aux2 = pd.concat([seats_index_LT, df_y_LT], axis=1)
+                aux2 = aux2[aux2["RelationID"]>0]
                 aux2 = aux2.sort_values(by = [1], ascending = False).head(3)
                 aux2["Preferencia"] = "Calidad"
                 
@@ -164,6 +166,7 @@ def main():
                 y_pred_country = random.predict_proba(df)
                 df_y_country = pd.DataFrame(y_pred_country)
                 aux3 = pd.concat([seats_country, df_y_country], axis=1)
+                aux3 = aux3[aux3["RelationID"]>0]
                 aux3 = aux3.sort_values(by = [1], ascending = False).head(1)
                 aux3["Preferencia"] = "Pais"
                 
